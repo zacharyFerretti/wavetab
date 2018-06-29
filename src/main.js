09c6a4f5f8d4cbe76b98f75d3d90d462c78cb5da
Liz Wigglesworth <lizwigg6@gmail.com>
2018-06-29 15:06:24 -0500

    Move all gradients to json format

--- src/main.js
+++ src/main.js
@@ -16,66 +16,6 @@ var randNum = 0;
 // storage object for options
 var options = {};
 
-// a bunch of these are from https://webgradients.com/
-var colorPairs = [
-	["#a18cd1", "#fbc2eb"], // 5 purple
-	["#ff9a9e", "#fecfef"], // 6 pink
-	["#fbc2eb", "#a6c1ee"], // 7 blue, purple
-	["#8db8ff", "#a1e1ff"], // 8 blue
-	["#bbff6d", "#7de08b"], // 9 green
-	["#43e97b", "#38f9d7"], // 10 green, teal
-	["#5ee7df", "#b490ca"], // 11 blue, purple
-	["#667eea", "#764ba2"], // 12 deep purple
-	["#c995f0", "#dbcfff"], // 13 lighter purple
-	["#6a11cb", "#2575fc"], // 14 deep blue
-	["#fffa86", "#ffdd25"], // 15 yellow, orange
-	["#d686ff", "#ffdd25"], // 16 purple, orange
-	["#f33f58", "#ffdd25"], // 17 red, orange
-	["#f33f58", "#8d25ff"], // 18 red-pink, purple
-	["#3ff36e", "#8d25ff"], // 19 green, purple
-	["#3ff3da", "#8d25ff"], // 20 light blue, purple
-	["#7dded1", "#61e2a1"], // muted blue,  muted green
-	["#d47dde", "#f5c070"], // muted purple,  muted orange
-	["#84fab0", "#8fd3f4"], // light green,  light blue (012 tempting azure)
-	["#8a81fe", "#51d7f4"], // light purple,  medium teal - brand color**
-	["#A1FFCE", "#ffd1e8"], // 25 https://uigradients.com/#Limeade
-	["#fceabb", "#f8b500"], // https://uigradients.com/#SunontheHorizon
-	["#42275a", "#734b6d"], // https://uigradients.com/#Mauve
-	["#e96443", "#904e95"], // https://uigradients.com/#GrapefruitSunset
-	["#4CA1AF", "#C4E0E5"], // https://uigradients.com/#Decent
-	["#eacda3", "#d6ae7b"], // 30 https://uigradients.com/#PaleWood
-	["#B24592", "#F15F79"], // https://uigradients.com/#Blush
-	["#c2e59c", "#64b3f4"], // https://uigradients.com/#GreenandBlue
-	["#73c646", "#8DC26F"], // modified https://uigradients.com/#LittleLeaf
-	["#673AB7", "#512DA8"], // https://uigradients.com/#DeepPurple
-	["#ee9ca7", "#ffdde1"], // 35 https://uigradients.com/#Piglet
-	["#83a4d4", "#b6fbff"], // https://uigradients.com/#Friday
-	["#9D50BB", "#6E48AA"], // https://uigradients.com/#Amethyst
-	["#FBD3E9", "#BB377D"], // https://uigradients.com/#Cherryblossoms
-	["#B993D6", "#8CA6DB"], // https://uigradients.com/#DirtyFog
-	["#00d2ff", "#3a7bd5"], // 40 https://uigradients.com/#Reef
-	["#DE6262", "#FFB88C"], // https://uigradients.com/#ALostMemory
-	["#f857a6", "#ff5858"], // https://uigradients.com/#DayTripper
-	["#5f2c82", "#49a09d"], // https://uigradients.com/#CalmDarya
-	["#24C6DC", "#514A9D"], // https://uigradients.com/#Mantle
-	["#026471", "#71B280"], // 45 https://uigradients.com/#Moss
-	["#ff9a9e", "#fad0c4"], // https://digitalsynopsis.com/design/beautiful-color-gradients-backgrounds/
-	["#ffecd2", "#fcb69f"], //
-	["#ff9a9e", "#fecfef"], //
-	["#a1c4fd", "#c2e9fb"], //
-	["#667eea", "#764ba2"], // 50
-	["#89f7fe", "#66a6ff"], //
-	["#48c6ef", "#6f86d6"], //
-	["#a6f3fe", "#f6c4e2"], // modified
-	["#a3bded", "#6991c7"], //
-	["#ff758c", "#ff7eb3"], // 55
-	["#96deda", "#50c9c3"], //
-	["#B7F8DB", "#50A7C2"], //
-	["#9c77e9", "#03c6ca"], //
-	["#53b4cb", "#6c55b3"], //
-	["#34f0e0", "#dcf002"] // 60
-];
-
 function updateTime() {
 	chrome.storage.local.get("use24HourTime", function(items) {
 		if (!items.use24HourTime) {
